9a5c7ff647fa44dc63367f28bb27a555a6ad6ae2
Dmitry <samoulovdima@mail.ru>
2025-05-26 20:49:41 +0500

    Connecting the user model

--- core/core/settings.py
+++ core/core/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Подключение модели пользователя
+AUTH_USER_MODEL = 'main.User'
